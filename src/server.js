0018e6321c9d3f3368dd166923076ec7fe06492a
Frank Lai <frank810618@gmail.com>
2020-09-09 15:08:18 +0800

    minor adjustments of server.js

--- src/server.js
+++ src/server.js
@@ -1,14 +1,16 @@
-const express = require('express');
-const SocketServer = require('ws').Server;
+//import express 和 ws 套件
+const express = require('express')
+const SocketServer = require('ws').Server
 
-const PORT = 3000;
+//指定開啟的 port
+const PORT = 3000
 
-const server = express.listen(PORT, () => {
-    console.log(`Listening on ${PORT}`);
-});
+//創建 express 的物件，並綁定及監聽 3000 port ，且設定開啟後在 console 中提示
+const server = express()
+    .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 //將 express 交給 SocketServer 開啟 WebSocket 的服務
-const wss = new SocketServer({ server })
+const wss = new SocketServer({ server });
 
 //當 WebSocket 從外部連結時執行
 wss.on('connection', ws => {
@@ -19,5 +21,5 @@ wss.on('connection', ws => {
     //當 WebSocket 的連線關閉時執行
     ws.on('close', () => {
         console.log('Close connected')
-    })
-})
+    });
+});
